8bfe904612116309f3b9a8564001cd342c568a32
edoardoMalaspina <e.malaspina@studenti.unipi.it>
2023-06-21 17:17:50 +0200

    modified photonicSigmoid.py

--- nnabla_nas/module/photonicSigmoid.py
+++ nnabla_nas/module/photonicSigmoid.py
@@ -24,11 +24,8 @@ class PhotonicSigmoid(Module):
         self._inplace = inplace
 
     def call(self, input):
-        #result = ()
-        #tmp = (1.005 + (0.06 - 1.005) / (1. + F.exp((input - 0.145) / 0.073)))
-        #result += (tmp,)
-        #return nn.Variable(result)
-        return (F.exp((input - 0.145)+1) )* 1/(F.exp((input - 0.145)+1)*(F.exp((input - 0.145)+1)))*(1.005 + (0.06 - 1.005))
+        result = F.log(F.exp((1.005 + (0.06 - 1.005) / (1. + F.exp((input - 0.145) / 0.073)))))
+        return result
 
     def extra_repr(self):
         return f'inplace={self._inplace}'
