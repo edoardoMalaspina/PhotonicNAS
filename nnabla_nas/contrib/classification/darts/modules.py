3a9830ba0b07634d38de89547199d66be1f7fa58
giuseppeaniello <giuseppe.aniello98@gmail.com>
2023-06-21 10:43:40 +0200

    modify candidates

--- nnabla_nas/contrib/classification/darts/modules.py
+++ nnabla_nas/contrib/classification/darts/modules.py
@@ -19,13 +19,13 @@ import nnabla.functions as F
 from .... import module as Mo
 
 CANDIDATES = OrderedDict([
-    ('dil_conv_3x3', lambda c, s: DDSConv(c, c, (3, 3), (2, 2), (s, s))),
+    #('dil_conv_3x3', lambda c, s: DDSConv(c, c, (3, 3), (2, 2), (s, s))),
     ('experimental1_3x3', lambda c, s: Experimental1(c, c, (3, 3), (2, 2), (s, s))),
     ('experimental2_3x3', lambda c, s: Experimental2(c, c, (3, 3), (2, 2), (s, s))),
     ('experimental1_2x2', lambda c, s: Experimental1(c, c, (2, 2), (1, 1), (s, s))),
     ('experimental2_3x3', lambda c, s: Experimental2(c, c, (2, 2), (1, 1), (s, s))),
 
-    ('sep_conv_3x3', lambda c, s: SepConv(c, c, (3, 3), (1, 1), (s, s))),
+   # ('sep_conv_3x3', lambda c, s: SepConv(c, c, (3, 3), (1, 1), (s, s))),
     #('max_pool_3x3', lambda c, s: Mo.MaxPool((3, 3), stride=(s, s), pad=(1, 1))),
     ('avg_pool_3x3', lambda c, s: Mo.AvgPool((3, 3), stride=(s, s), pad=(1, 1))),
     ('skip_connect', lambda c, s: FactorizedReduce(c, c) if s > 1
