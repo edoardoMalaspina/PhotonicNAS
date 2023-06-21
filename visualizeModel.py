0f23915d55079ab68fdf5d6a4d6f148f80538d40
edoardoMalaspina <e.malaspina@studenti.unipi.it>
2023-06-21 21:33:17 +0200

    modified script

--- visualizeModel.py
+++ visualizeModel.py
@@ -5,6 +5,6 @@ from nnabla.utils.save import save
 from nnabla.utils.nnp_graph import NnpLoader
 
 nnp = NnpLoader('log/classification/darts/cifar10/search/results.nnp')
-net = nnp.get_network("SearchNet", batch_size=1)
+net = nnp.get_network("classification/darts/cifar10/search")
 list = net.get_network_names()
 print(list)
