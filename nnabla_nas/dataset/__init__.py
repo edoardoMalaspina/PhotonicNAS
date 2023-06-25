82853020292472a825a58009c78c75ad46c437ba
giuseppeaniello <giuseppe.aniello98@gmail.com>
2023-06-25 17:40:46 +0200

    ADD MNIST dataset

--- nnabla_nas/dataset/__init__.py
+++ nnabla_nas/dataset/__init__.py
@@ -14,6 +14,7 @@
 
 from . import cifar10
 from . import csv
+from . import mnist
 try:
     from . import imagenet
 except ImportError:
@@ -22,4 +23,4 @@ except ImportError:
     logger.warning(
         '  If you want to use nnabla_nas.dataset.imagenet, please install nvidia-dali-cuda???.')
 
-__all__ = ['cifar10', 'imagenet', 'csv','MNIST']
+__all__ = ['cifar10', 'imagenet', 'csv','mnist']
